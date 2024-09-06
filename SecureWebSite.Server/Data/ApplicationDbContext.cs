d0cf7df333aae9f758cd3f2b1979353a326d98b9
Mert1026 <mertelsenev10@gmail.com>
2024-09-07 01:29:19 +0300

    Connecting the DB

--- SecureWebSite.Server/Data/ApplicationDbContext.cs
+++ SecureWebSite.Server/Data/ApplicationDbContext.cs
@@ -4,8 +4,31 @@ using SecureWebSite.Server.Models;
 
 namespace SecureWebSite.Server.Data
 {
-		public class ApplicationDbContext : IdentityDbContext<User>
-		{
-				public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
-		}
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public ApplicationDbContext()
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(@"Server=DESKTOP-N2P1BBN\SQLEXPRESS;Database=FMA_TEST2;Integrated Security=True;TrustServerCertificate=True;");
+            }
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
 }
